e475d33f5d9dbc859e92a8a821f2634c78c084f6
jashina nina <naiashina@edu.hse.ru>
2026-04-10 21:01:42 +0000

    Upload New File

---
+++ TextButtonImageScreenConfig.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct TextButtonImageScreenConfig {
+    let image: String?
+    let title: String?
+    let text: String?
+    //let buttonText: String?
+    let buttons: [ButtonConfig]
+    
+}
