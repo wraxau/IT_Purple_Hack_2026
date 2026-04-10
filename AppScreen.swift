36818c29510025937213f2324c2c7d0f3c13cf49
jashina nina <naiashina@edu.hse.ru>
2026-04-10 20:56:22 +0000

    Upload New File

---
+++ AppScreen.swift
@@ -0,0 +1,12 @@
+enum AppScreen {
+    case welcomeScreen
+    case choosePlotScreen
+    case basicScreen(TextButtonImageScreenConfig)
+    case howItWorksScreen(HowItWorksScreen)
+    case finalMoneyScreen
+    case playerStatusScreen
+    case themeEndingMenu(String)
+
+}
+
+
