fde928eaa5d24eab7e0b9a86bf6a9200c3806032
Nina Iashina <145937151+wraxau@users.noreply.github.com>
2026-04-27 15:29:41 +0300

    Delete WhatIfConfig.swift

--- WhatIfConfig.swift
+++
@@ -1,9 +0,0 @@
-struct WhatIfAlternative {
-    let label: String
-    let actionID: String  // ID для расчёта альтернативы
-}
-
-struct WhatIfConfig {
-    let title: String
-    let alternatives: [WhatIfAlternative]
-}
